ecc48fa3621d26a4dbc1ef5cff1b3db638660c18
Phil Roth <phil.roth077@gmail.com>
2019-06-06 00:31:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,12 @@
 def using_push(array, element)
   array.push(element)
 end
+def using_unshift(array, element)
+  array.unshift(element)
+end
+def using_pop(array)
+  array.pop
+end
+def using_push(array, element)
+  array.push(element)
+end
